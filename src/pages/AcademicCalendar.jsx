8cbca54f3aaf05b5f32c1e7bdd1fced10d9fd922
habiba khan <habibak2022@gmail.com>
2026-05-29 02:42:50 +0000

    File changes

--- src/pages/AcademicCalendar.jsx
+++ src/pages/AcademicCalendar.jsx
@@ -693,8 +693,7 @@ export default function AcademicCalendar() {
           </div>
         </div>
         </div>
-        )}
-      </div>
+      )}
 
       {/* Bottom Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
